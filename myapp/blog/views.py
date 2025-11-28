0be743a2cbfda7c71e1cd8654f2b7d574378434c
balaganapathiA <anandabalan333555777@gmail.com>
2025-11-28 23:13:14 +0530

    created data in daty base from here and list that data in render page

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import logging
-posts = [
-        {'title':'post1','content':'post 1 content'},
-        {'title':'post2','content':'post 2 content'},
-        {'title':'post3','content':'post 3 content'},
-        {'title':'post4','content':'post 4 content'},
-        ]
+from .models import Post
+# posts = [
+#         {'title':'post1','content':'post 1 content'},
+#         {'title':'post2','content':'post 2 content'},
+#         {'title':'post3','content':'post 3 content'},
+#         {'title':'post4','content':'post 4 content'},
+#         ]
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, World!")
@@ -18,10 +19,12 @@ def redirected_page(request):
     return HttpResponse("url redirected succesfully")
 
 def render_page(request):
+    posts=Post.objects.all()
     return render(request,"index.html",{'posts':posts})
 
 def detail_page(request,id):
-    post_details = next((item for item in posts if item["title"] == id),None)
+    # post_details = next((item for item in posts if item["id"] == id),None)
+    post_details=Post.objects.get(pk=id)
     log = logging.getLogger("testing")
     log.debug(f'id {post_details} {id} of detail page')
     return render(request,"detail.html",{"post_details":post_details})
