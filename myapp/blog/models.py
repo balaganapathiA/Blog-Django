e897a0cc73ef4235c3861662645b0f8b657ad9fd
balaganapathiA <anandbalan333555777@gmail.com>
2025-11-30 20:35:55 +0530

    posts lists with multiple categories and secret key added in env

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -4,12 +4,21 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+# Category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) :
+        return self.name
+        
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     img_url = models.URLField(null=True)
     created_at = models.DateField(auto_now_add=True)
     slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -17,10 +26,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-# Category
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self) :
-        return self.name
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
+print(os.environ.get("SECRET_KEY"),"env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
