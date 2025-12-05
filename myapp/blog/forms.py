e772a2ca59f9354c4b16f42d730ff28c802ed5ce
balaganapathiA <anandbalan333555777@gmail.com>
2025-12-05 21:47:03 +0530

    custom validation and non field errors

--- myapp/blog/forms.py
+++ myapp/blog/forms.py
@@ -19,13 +19,13 @@ class RegisterForm(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'password']
 
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         password = cleaned_data.get("password")
-#         password_confirm = cleaned_data.get("password_confirm")
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
 
-#         if password and password_confirm and password != password_confirm:
-#             raise forms.ValidationError("Passwords do not match.")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Password Same aa illa.")
 
 # class LoginForm(forms.Form):
 #     username = forms.CharField(label='username', max_length=100, required=True)
