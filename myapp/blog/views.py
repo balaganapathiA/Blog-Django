684d5e103c4ec162e2b36d61ca3955cdc85d403e
balaganapathiA <anandbalan333555777@gmail.com>
2025-12-10 21:30:29 +0530

    reset password email template and email sending setup

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -7,6 +7,16 @@ from django.contrib import messages
 from django.contrib.messages import get_messages
 from .forms import ContactForm,RegisterForm,LoginForm,ForgotPasswordForm
 from django.contrib.auth import authenticate,login,logout
+
+
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes 
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import Group
 # posts = [
 #         {'title':'post1','content':'post 1 content'},
 #         {'title':'post2','content':'post 2 content'},
@@ -121,5 +131,24 @@ def forgot_page(request):
     if request.method == 'POST':
         #form
         form = ForgotPasswordForm(request.POST)
-    return render(request,'forgot_password.html')
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            user = User.objects.get(email=email)
+            #send email to reset password
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            current_site = get_current_site(request)
+            domain = current_site.domain
+            subject = "Reset Password Requested"
+            message = render_to_string('reset_password_email.html', {
+                'domain': domain,
+                'uid': uid,
+                'token': token
+            })
+
+            send_mail(subject, message, 'noreply@jvlcode.com', [email])
+            messages.success(request, 'Email has been sent')
+
+
+    return render(request,'forgot_password.html', {'form': form})
     
